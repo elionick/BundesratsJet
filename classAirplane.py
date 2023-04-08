1cc8879114f7b36e4a64009507cfd1fabf0014b3
Elio Hahn <elio.hahn@rubigen.ch>
2023-04-09 01:17:42 +0200

    Setting up project

---
+++ classAirplane.py
@@ -0,0 +1,10 @@
+class Airplane:
+    
+    def __init__(self, name, reg, code):
+        self.name = name
+        self.reg = reg
+        self.code = code
+
+    def testMethod(self):
+        pass
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from classAirplane import *
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
